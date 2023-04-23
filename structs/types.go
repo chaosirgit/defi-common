a43c9fb6425576a116742a0df2409909b8cda419
Ethan Kuo <mrnicolascoge@gmail.com>
2023-04-23 15:05:37 +0800

    add userbotsetting struct

--- structs/types.go
+++ structs/types.go
@@ -82,3 +82,111 @@ func (t TransactionData) MarshalJSON() ([]byte, error) {
 		BlockNumber: t.BlockNumber.String(),
 	})
 }
+
+// 用户配置
+type UserBotSetting struct {
+	ID              int64          `json:"id"`
+	WhaleAddress    common.Address `json:"whale_address"`
+	WhaleName       string         `json:"whale_name"`
+	ChainID         *big.Int       `json:"chain_id"`
+	ListenBase      string         `json:"listen_base"`
+	MinWethAmount   *big.Int       `json:"min_weth_amount"`
+	MinBusdAmount   *big.Int       `json:"min_busd_amount"`
+	MinUsdtAmount   *big.Int       `json:"min_usdt_amount"`
+	MinUsdcAmount   *big.Int       `json:"min_usdc_amount"`
+	SellAllWethLine *big.Int       `json:"sell_all_weth_line"`
+	SellAllBusdLine *big.Int       `json:"sell_all_busd_line"`
+	SellAllUsdtLine *big.Int       `json:"sell_all_usdt_line"`
+	SellAllUsdcLine *big.Int       `json:"sell_all_usdc_line"`
+	StopAt          int64          `json:"stop_at"`
+	IsSellAll       int8           `json:"is_sell_all"`
+	StopWin         int64          `json:"stop_win"`
+	StopWinMin      int64          `json:"stop_win_min"`
+	AvoidHoneyPot   int8           `json:"avoid_honey_pot"`
+	IsZero          int8           `json:"is_zero"`
+	PrivateKey      string         `json:"private_key"`
+}
+
+func (s *UserBotSetting) UnmarshalJSON(data []byte) error {
+	aux := &struct {
+		ID              int64  `json:"id"`
+		WhaleAddress    string `json:"whale_address"`
+		WhaleName       string `json:"whale_name"`
+		ChainID         int64  `json:"chain_id"`
+		ListenBase      string `json:"listen_base"`
+		MinWethAmount   string `json:"min_weth_amount"`
+		MinBusdAmount   string `json:"min_busd_amount"`
+		MinUsdtAmount   string `json:"min_usdt_amount"`
+		MinUsdcAmount   string `json:"min_usdc_amount"`
+		SellAllWethLine string `json:"sell_all_weth_line"`
+		SellAllBusdLine string `json:"sell_all_busd_line"`
+		SellAllUsdtLine string `json:"sell_all_usdt_line"`
+		SellAllUsdcLine string `json:"sell_all_usdc_line"`
+		StopAt          int64  `json:"stop_at"`
+		IsSellAll       int8   `json:"is_sell_all"`
+		StopWin         int64  `json:"stop_win"`
+		StopWinMin      int64  `json:"stop_win_min"`
+		AvoidHoneyPot   int8   `json:"avoid_honey_pot"`
+		IsZero          int8   `json:"is_zero"`
+		PrivateKey      string `json:"private_key"`
+	}{}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	chainId := new(big.Int).SetInt64(aux.ChainID)
+
+	minWeth, ok := new(big.Int).SetString(aux.MinWethAmount, 10)
+	if !ok {
+		return fmt.Errorf("failed to unmarshal MinWethAmount from string: %s", aux.MinWethAmount)
+	}
+	minBusd, ok := new(big.Int).SetString(aux.MinBusdAmount, 10)
+	if !ok {
+		return fmt.Errorf("failed to unmarshal MinBusdAmount from string: %s", aux.MinBusdAmount)
+	}
+	minUsdt, ok := new(big.Int).SetString(aux.MinUsdtAmount, 10)
+	if !ok {
+		return fmt.Errorf("failed to unmarshal MinUsdtAmount from string: %s", aux.MinUsdtAmount)
+	}
+	minUsdc, ok := new(big.Int).SetString(aux.MinUsdcAmount, 10)
+	if !ok {
+		return fmt.Errorf("failed to unmarshal MinUsdcAmount from string: %s", aux.MinUsdcAmount)
+	}
+	sellWeth, ok := new(big.Int).SetString(aux.SellAllWethLine, 10)
+	if !ok {
+		return fmt.Errorf("failed to unmarshal SellAllWethLine from string: %s", aux.SellAllWethLine)
+	}
+	sellBusd, ok := new(big.Int).SetString(aux.SellAllBusdLine, 10)
+	if !ok {
+		return fmt.Errorf("failed to unmarshal SellAllBusdLine from string: %s", aux.SellAllBusdLine)
+	}
+	sellUsdt, ok := new(big.Int).SetString(aux.SellAllUsdtLine, 10)
+	if !ok {
+		return fmt.Errorf("failed to unmarshal SellAllUsdtLine from string: %s", aux.SellAllUsdtLine)
+	}
+	sellUsdc, ok := new(big.Int).SetString(aux.SellAllUsdcLine, 10)
+	if !ok {
+		return fmt.Errorf("failed to unmarshal SellAllUsdcLine from string: %s", aux.SellAllUsdcLine)
+	}
+
+	s.ID = aux.ID
+	s.WhaleAddress = common.HexToAddress(aux.WhaleAddress)
+	s.WhaleName = aux.WhaleName
+	s.ChainID = chainId
+	s.ListenBase = aux.ListenBase
+	s.MinWethAmount = minWeth
+	s.MinBusdAmount = minBusd
+	s.MinUsdtAmount = minUsdt
+	s.MinUsdcAmount = minUsdc
+	s.SellAllWethLine = sellWeth
+	s.SellAllBusdLine = sellBusd
+	s.SellAllUsdtLine = sellUsdt
+	s.SellAllUsdcLine = sellUsdc
+	s.StopAt = aux.StopAt
+	s.IsSellAll = aux.IsSellAll
+	s.StopWin = aux.StopWin
+	s.StopWinMin = aux.StopWinMin
+	s.AvoidHoneyPot = aux.AvoidHoneyPot
+	s.IsZero = aux.IsZero
+	s.PrivateKey = aux.PrivateKey
+	return nil
+}
