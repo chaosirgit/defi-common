7729a6fb11d0b176b8e692908059528156ceb58b
Ethan Kuo <mrnicolascoge@gmail.com>
2023-04-26 14:00:16 +0800

    update purchaseInfo method

--- structs/purchase_info.go
+++ structs/purchase_info.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis/v8"
+	"strings"
 )
 
 type PurchaseInfo struct {
@@ -19,6 +20,7 @@ func (purchase *PurchaseInfo) getKey() string {
 	return fmt.Sprintf("SettingFirstPurchase:%s", purchase.UserBotSettingID)
 }
 
+// 第一次买入，存入 redis
 func (purchase *PurchaseInfo) SavePurchaseInfoToRedis(rdb *redis.Client) error {
 	ctx := context.Background()
 
@@ -46,9 +48,17 @@ func (purchase *PurchaseInfo) SavePurchaseInfoToRedis(rdb *redis.Client) error {
 		return err
 	}
 
+	// 将 UserBotSettingID 添加到 Redis 集合中
+	tokenKey := fmt.Sprintf("FirstPurchaseToken:%s", strings.ToLower(purchase.TokenAddress))
+	err = rdb.SAdd(ctx, tokenKey, purchase.UserBotSettingID).Err()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
+// 获取 该用户下的所有购买的币
 func (purchase *PurchaseInfo) GetAllTokens(rdb *redis.Client) ([]string, error) {
 	ctx := context.Background()
 
@@ -60,4 +70,51 @@ func (purchase *PurchaseInfo) GetAllTokens(rdb *redis.Client) ([]string, error) 
 	}
 
 	return tokenAddresses, nil
+}
+
+// 获取平台所有已买入的代币
+func GetAllUniqueTokensBought(rdb *redis.Client) ([]string, error) {
+	ctx := context.Background()
+
+	// 使用一个 map 来存储去重后的代币地址
+	uniqueTokens := make(map[string]bool)
+
+	// 获取所有以 "FirstPurchaseToken:" 开头的 key
+	keys, err := rdb.Keys(ctx, "FirstPurchaseToken:*").Result()
+	if err != nil {
+		return nil, err
+	}
+
+	// 遍历所有的 FirstPurchaseToken:<TokenAddress> key
+	for _, key := range keys {
+		// 从 key 中提取 TokenAddress
+		tokenAddress := strings.TrimPrefix(key, "FirstPurchaseToken:")
+
+		// 将 TokenAddress 添加到 uniqueTokens map 中
+		uniqueTokens[tokenAddress] = true
+	}
+
+	// 将 uniqueTokens map 转换为字符串切片
+	result := make([]string, 0, len(uniqueTokens))
+	for tokenAddress := range uniqueTokens {
+		result = append(result, tokenAddress)
+	}
+
+	return result, nil
+}
+
+// 获取某个 代币下的所有买入用户
+func GetUserBotSettingIDsForToken(rdb *redis.Client, tokenAddress string) ([]string, error) {
+	ctx := context.Background()
+
+	// 根据 tokenAddress 构建 Redis 集合 key
+	tokenKey := fmt.Sprintf("FirstPurchaseToken:%s", tokenAddress)
+
+	// 从 Redis 集合中获取所有 UserBotSettingID
+	userBotSettingIDs, err := rdb.SMembers(ctx, tokenKey).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return userBotSettingIDs, nil
 }
